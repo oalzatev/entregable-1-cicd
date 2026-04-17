44cd38da53a38f00069fa914d7b032cd42a1d38d
oalzatev <oalzatev@gmail.com>
2026-04-17 15:50:17 -0500

    Completar el workflow del entregable 1

---
+++ mi_script.py
@@ -0,0 +1,13 @@
+# mi_script.py
+import platform
+import datetime
+
+AUTORES = "???"  # IMPORTANTE: Reemplaza con los nombres completos de los estudiantes que participaron en la entrega, separados por comas (debe coincidir exactamente con los que pusiste en el workflow)
+
+def main():
+    print(f"Autores: {Oswaldo Alzarte}")
+    print(f"Sistema Operativo: {platform.system()} {platform.release()}")
+    print(f"Fecha y hora desde Python: {datetime.datetime.now()}")
+
+if __name__ == "__main__":
+    main()
